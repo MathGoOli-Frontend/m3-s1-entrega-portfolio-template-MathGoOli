e37c19762ddb3aa958e2fa5baece36b98456e5c6
mathGoOli <matheuss456@hotmail.com>
2023-05-27 17:27:31 -0300

    fix technologiesCard

--- src/fragments/TecnologyCard/index.jsx
+++ src/fragments/TecnologyCard/index.jsx
@@ -4,7 +4,7 @@ export const TecnologyCard = (tech) => {
     return(
         <li className={style.card}>
             <img src={tech.tech.img} alt={tech.tech.name} />
-            <h3>{tech.tech.name}</h3>
+            <h3 className="title-3">{tech.tech.name}</h3>
         </li>
     )
 }
